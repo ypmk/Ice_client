72eebe333bbe88beb0f37ec6de9619368991fab1
ypmk <kopylovaksusha123@gmail.com>
2024-05-21 23:46:43 +0300

    Отредактирована страница коллекци (Удалено изображение сверху)

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -12,13 +12,7 @@ const CollectionDetails = async ({
 
   return (
     <div className="px-10 py-5 flex flex-col items-center gap-8">
-      <Image
-        src={collectionDetails.image}
-        width={1500}
-        height={1000}
-        alt="collection"
-        className="w-full h-[400px] object-cover rounded-xl"
-      />
+
       <p className="text-heading3-bold text-grey-2">{collectionDetails.title}</p>
       <p className="text-body-normal text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
       <div className="flex flex-wrap gap-16 justify-center">
